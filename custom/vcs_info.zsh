49eb38befc4d0258fb886a52598931f8b2ece53f
Daniel Hahler <git@thequod.de>
2010-05-27 21:52:43 +0200

    vcs_info: enable: put bzr before git for bzr repos in ~.

--- custom/vcs_info.zsh
+++ custom/vcs_info.zsh
@@ -1,3 +1,3 @@
 # directory based VCS before repo based ones (e.g. CVS in $HOME, the latter using Git)
-zstyle ':vcs_info:*' enable cvs svn git bzr
+zstyle ':vcs_info:*' enable cvs svn bzr git
 zstyle ':vcs_info:bzr:*' use-simple true
