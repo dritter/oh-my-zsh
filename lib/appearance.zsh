39297399472326f0b2f298b2fa2a1e4cf0b0a1e8
Daniel Hahler <git@thequod.de>
2010-05-31 00:41:25 +0200

    Export and setup LS_COLORS (linux), too.

--- lib/appearance.zsh
+++ lib/appearance.zsh
@@ -1,13 +1,16 @@
 # ls colors
 autoload colors; colors;
 export LSCOLORS="Gxfxcxdxbxegedabagacad"
-#export LS_COLORS
+export LS_COLORS
 
 # Enable ls colors
 if [ "$DISABLE_LS_COLORS" != "true" ]
 then
   # Find the option for using colors in ls, depending on the version: Linux or BSD
   ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
+
+	# setup LS_COLORS (Linux)
+	eval `dircolors -b`
 fi
 
 #setopt no_beep
