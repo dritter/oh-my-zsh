e0826576e7c40402c20818de96c1268ad9dd3f87
Daniel Hahler <git@thequod.de>
2011-02-17 23:12:02 +0100

    zsh: improve shell title suffix/prefix (OpenVZ related)

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -1,20 +1,21 @@
 ## fixme, i duplicated this in xterms - oops
 function title {
-  local SUFFIX RELPWD
+  local PREFIX SUFFIX RELPWD
   if [[ $TERM == screen* ]]; then
     # Get OpenVZ container ID (/proc/bc is only on the host):
     if [[ -r /proc/user_beancounters ]]; then
       if [[ ! -d /proc/bc ]]; then
         # container
-        SUFFIX=" [$(hostname)#$(sed -n 3p /proc/user_beancounters | cut -f1 -d: | tr -d '[:space:]'):$PWD]"
+        PREFIX="[$(hostname)#$(sed -n 3p /proc/user_beancounters | cut -f1 -d: | tr -d '[:space:]')] "
+        SUFFIX=" ($PWD)"
       elif [[ $(pwd -P) == /var/lib/vz/private/[0-9]* ]]; then
         # HN, in container dir
         RELPWD=${$(pwd -P)#/var/lib/vz/private/}
-        SUFFIX=" [HN:${RELPWD%%/*}~${RELPWD##[[:digit:]]##/#}]"
+        SUFFIX=" (HN:${RELPWD%%/*}~${RELPWD##[[:digit:]]##/#})"
       fi
     fi
     # Use these two for GNU Screen:
-    print -nR $'\033k'"${(f)*}$CTID${SUFFIX- [$PWD]}"$'\033'\\\
+    print -nR $'\033k'"${PREFIX}${(f)*}${SUFFIX:- [$PWD]}"$'\033'\\\
     # xterm title: gets updated via screen hardstatus
     # print -nR $'\033]0;'${(f)2}$'\a'
   elif [[ $TERM == xterm* || $TERM == "rxvt" ]]; then
