698b31f62f451f517dcc5b09e28d318515825c13
Pat Regan <thehead@patshead.com>
2011-04-12 17:41:09 -0400

    Optimize plugin loading so that only one call to compinit is required

--- plugins/github/github.plugin.zsh
+++ plugins/github/github.plugin.zsh
@@ -1,6 +1,3 @@
-# add github completion function to path
-fpath=($ZSH/plugins/github $fpath)
-
 # Setup hub alias for git, if it is available, see http://github.com/defunkt/hub
 if [ "$commands[(I)hub]" ]; then
 	# eval `hub alias -s zsh`
