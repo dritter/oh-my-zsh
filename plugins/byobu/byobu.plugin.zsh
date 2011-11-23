a65095918d2b6bb388c4803b7a89e2496211e3f7
Daniel Hahler <git@thequod.de>
2011-11-23 20:23:48 +0100

    Update byobu plugin to r1787 (4.50 pre-release).

--- plugins/byobu/byobu.plugin.zsh
+++ plugins/byobu/byobu.plugin.zsh
@@ -1,4 +1,11 @@
-# Add byobu binaries to the beginning of $PATH
-path=($0:h/bin $path)
+# Set prefix to byobu (this should work across different shells, e.g. bash, too)
+if [ -n "$BASH_SOURCE" ]; then
+  SELF_PATH="$BASH_SOURCE" # required for "source .." in bash
+else
+  SELF_PATH="$0"
+fi
+export BYOBU_PREFIX=$(dirname $(readlink -f "$SELF_PATH"))
+# $( cd -P -- "$(dirname -- "$(command -v -- "$SELF_PATH")")" && pwd -P )
 
-export BYOBU_PREFIX="$0:h"
+# Add byobu binaries to the beginning of $PATH
+PATH="$BYOBU_PREFIX/bin:$PATH"
