6577deb2f2b3d2bf0bd119925e5ba67a0cb9210b
Göran Gustafsson <gustafsson.g@gmail.com>
2011-12-03 13:18:37 +0100

    Removed duplicate setting and sorted the remaining

--- lib/history.zsh
+++ lib/history.zsh
@@ -3,15 +3,10 @@ HISTFILE=$HOME/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt hist_ignore_dups # ignore duplication command history list
-setopt share_history # share command history data
-
-setopt no_hist_verify
-setopt inc_append_history
 setopt extended_history
 setopt hist_expire_dups_first
+setopt no_hist_ignore_dups
 setopt hist_ignore_space
-
-# Do not share history automatically, but import it manually using "fc -RI" when needed
-unsetopt SHARE_HISTORY
-setopt INC_APPEND_HISTORY
+setopt no_hist_verify
+setopt inc_append_history
+setopt no_share_history # Do not share history automatically, but import it manually using "fc -RI" when needed
