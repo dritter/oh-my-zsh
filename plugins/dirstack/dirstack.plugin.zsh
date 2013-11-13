98ab190a49ff82714347288cacf777823ffdfcbe
Daniel Hahler <git@thequod.de>
2013-11-13 04:10:51 +0100

    dirstack plugin: go back to symlinked dir on init
    Also use add-zsh-hook.

--- plugins/dirstack/dirstack.plugin.zsh
+++ plugins/dirstack/dirstack.plugin.zsh
@@ -16,12 +16,22 @@ if [[ -f ${DIRSTACKFILE} ]] && [[ ${#dirstack[*]} -eq 0 ]] ; then
     # "cd -" won't work after login by just setting $OLDPWD, so
     if [[ ${${dirstack[1]}[1,5]} != '/mnt/' ]]; then # skip any /mnt entries, which might hang (e.g. sshfs/cifs without network)
         if [[ -d $dirstack[1] ]]; then
-            cd $dirstack[1] && cd $OLDPWD
+            # if $PWD is the most recent dirstack entry, but resolved, change back and forth
+            # This happens when opening a new tab in gnome-terminal, which appears to resolve the symlink.
+            if [[ $PWD != $dirstack[1] ]] && [[ $(readlink -f $PWD) == $(readlink -f $dirstack[1]) ]]; then
+                # save dirstack[1], which changes after the first cd!
+                local -h d1=$dirstack[1]
+                cd -qL $dirstack[2] && cd -qL $d1
+            else
+                cd -qL $dirstack[1] && cd -qL $OLDPWD
+            fi
         fi
     fi
 fi
 
-chpwd() {
+autoload -U add-zsh-hook
+add-zsh-hook chpwd _zsh_dirstack_chpwd_hook
+_zsh_dirstack_chpwd_hook() {
     local -ax my_stack
     my_stack=( ${PWD} ${dirstack} )
     if is42 ; then
