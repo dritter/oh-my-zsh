d654e80de6364f5d2adf7ed45703c8f9b37b0d4c
Daniel Hahler <git@thequod.de>
2015-01-17 06:59:47 +0100

    grep: --color=auto might not be available (busybox)

--- lib/grep.zsh
+++ lib/grep.zsh
@@ -3,13 +3,16 @@ VCS_FOLDERS="{.bzr,.cvs,.git,.hg,.svn}"
 
 _setup_grep_alias() {
     local GREP_OPTIONS
-    # Color grep results.
-    GREP_OPTIONS=(--color=auto)
+    GREP_OPTIONS=()
 
     # Is grep argument $1 available?
     grep-flag-available() {
         echo | grep $1 "" >/dev/null 2>&1
     }
+    # Color grep results.
+    if grep-flag-available --color=auto; then
+        GREP_OPTIONS=(--color=auto)
+    fi
     if grep-flag-available --exclude-dir=.cvs; then
         GREP_OPTIONS+=(--exclude-dir=$VCS_FOLDERS)
     elif grep-flag-available --exclude=.cvs; then
